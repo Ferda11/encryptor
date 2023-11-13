815d49a1b5673f019d99883d922fb033a99a3a48
Fleurimond Ferda Carine <ferdacarinef@gmail.com>
2023-11-13 14:44:03 -0500

    setup commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='monpackage',
+     author='C.M Tech',
+    author_email='ferdacarinef@gmail.com',
+    description='package pour encryter et decrytper des mots',
+    url='',
+    version='0.1',
+    python_requires='>=3.10',
+    packages=["encryptor.py"],
+    
+)
